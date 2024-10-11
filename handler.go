7d035e241331834f1267624eeda60142d500fd40
josephyou <josephyou@tencent.com>
2024-10-11 16:37:28 +0800

    fix generate

--- handler.go
+++ handler.go
@@ -33,14 +33,26 @@ func generate(c *gin.Context) {
 	if ctrl != nil {
 		baseUrl := "generate"
 		c.Stream(func(w io.Writer) bool {
-			for data := range ctrl.Dispatching([]models.Request{req}, baseUrl) {
-				if len(data) > 0 {
-					w.Write(data)
-				} else {
+			dataChan := ctrl.Dispatching([]models.Request{req}, baseUrl)
+			for {
+				select {
+				case data, ok := <-dataChan:
+					if !ok {
+						return false
+					}
+					if len(data) > 0 {
+						_, err := w.Write(data)
+						if err != nil {
+							return false
+						}
+					} else {
+						return false
+					}
+				case <-c.Request.Context().Done():
 					return false
 				}
+
 			}
-			return true
 		})
 	} else {
 		c.JSON(http.StatusInternalServerError, gin.H{"message": "Controller is not initialized"})
